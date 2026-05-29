76a4c6d6b2d4933dc08af301fb8404022929f867
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-05-29 18:51:50 +0530

    add mongoDB so tasks dont get removed even after restarting our server

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose")
 
 
 const app = express();
-const items = []
-let workItems = [];
+// const items = []
+// let workItems = [];
 
 
 app.set("view engine", "ejs");
@@ -15,26 +15,75 @@ app.use(bodyParser.urlencoded({extended: true})) // must use to get text from 'h
 app.use(express.static("public")) // this includes every file from the public folder for eg. css file or any js file, and includes it to our server.
 
 
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+
+const itemSchema = new mongoose.Schema({
+  name: String
+})
+
+const item = mongoose.model("item", itemSchema)
+
+const item1 = new item({
+  name: "Welcome to your todolist"
+})
+const item2 = new item({
+  name: "Hit + button to add new task."
+})
+const item3 = new item({
+  name: "Hit checkbox to delete item."
+})
+
+const defaultItems = [item1, item2, item3]
+
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
-  
-  let day = date.getDate();
 
-  res.render("list", { 
-    listTitle: day, 
-    newListItems: items 
-  });
+  item.find({})
+    .then((foundItems) => {
+
+      if (foundItems.length === 0) {
+
+        item.insertMany(defaultItems)
+          .then(() => {
+            console.log("Successfully saved default items");
+            res.redirect("/");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+      } else {
+
+        res.render("list", {
+          listTitle: "Today",
+          newListItems: foundItems
+        });
+
+      }
+
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
 });
-
 app.post("/", (req, res) => {
-  let item = req.body.newItem;
-  if (item === "Work") {
-    workItems.push(item)
-    res.redirect("/work")
-  } else{
-    items.push(item)
-    res.redirect("/")
-  }
+  const itemName =  req.body.newItem;
 
+  const item = new item({
+    name: itemName
+  })
+
+
+  item.save()
+
+  res.redirect("/")
 });
 
 app.get("/work", (req,res)=>{
